59b7f4a6c5f31dfca64c7bb1dd635ed0e01de031
A. Samy <f.fallen45@gmail.com>
2012-10-05 15:03:18 +0000

    DERPITY DERP

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2012 fallen <f.fallen45@gmail.com>
  *
  * "THE BEER-WARE LICENSE" (Revision 42):
- * Sergey Lyubka wrote this file.  As long as you retain this notice you
+ * fallen wrote this file.  As long as you retain this notice you
  * can do whatever you want with this stuff. If we meet some day, and you think
  * this stuff is worth it, you can buy me a beer in return.
  */
